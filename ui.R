7431693c8bcb32ffe3db9e7223a1d7618b15675d
duican <104439069+duican@users.noreply.github.com>
2025-05-02 16:30:31 +0200

    Add files via upload

--- ui.R
+++ ui.R
@@ -32,16 +32,16 @@ shinyUI(
     
     dashboardSidebar(
       sidebarMenu(
-      menuItem("K-Means", tabName = "kmeans", icon = icon("home")),
-      menuItem("Hierarchical Clustering", tabName = "hierarchical", icon = icon("home")),
-      menuItem("Principal Component Analysis", tabName = "pca", icon = icon("home")),
+      menuItem("Our Tox K-Means", tabName = "toxkmeans", icon = icon("home")),
+      menuItem("Our Tox Hierarchical Clustering", tabName = "toxhierarchical", icon = icon("home")),
+      menuItem("Our Tox Principal Component", tabName = "toxpca", icon = icon("home")),
       menuItem("Toxicity Charts", tabName = "toxpi", icon = icon("home")),
       menuItem("Upload Data", tabName = "upload", icon = icon("calendar"))
     )),
     
     dashboardBody(
       tabItems(
-        tabItem(tabName = "kmeans",h4("K-Means"),
+        tabItem(tabName = "toxkmeans",h4("K-Means (applied to 141 chemicals)"),
                fluidRow(column(12,
                tabBox(id="tabchart1",width=12,
                       tabPanel("K-Means results",numericInput('number_cluster', 'Please choose the prefered number of clusters:', 4, min = 1, max = 10), plotOutput('plot1')),
@@ -49,7 +49,7 @@ shinyUI(
                       )
                )),
         ),
-        tabItem(tabName = "hierarchical", h4("Hierarchical Clustering"),
+        tabItem(tabName = "toxhierarchical", h4("Hierarchical Clustering (applied to 141 chemicals)"),
                 fluidRow(column(
                   height = 12,
                   width = 12
@@ -82,7 +82,7 @@ shinyUI(
                          ),
                        tabPanel("OrdinalNET",  
                                 fluidPage(
-                                  selectInput("user_slices", "Select the number of slices:", choices = c(7, 8, 9, 10, 11)),
+                                  selectInput("user_slices", "Please select number of chemical substances per slice (Info: The available sample sizes are 49, 56, 63, 70 and 77 and are calculated as the number of independent variables (7 have been chosen in the initial model and in the regression) multiplied with the number of chemical substances per slice (7, 8, 9, 10 or 11)):", choices = c(7, 8, 9, 10, 11)),
                                   textOutput("received_slices")
                                 ),
                                 actionButton("our_model2", "Run ord reg"),
@@ -90,7 +90,7 @@ shinyUI(
                        ),
                        tabPanel("Correlation values",  
                                 fluidPage(
-                                  selectInput("user_slices", "Select the number of slices:", choices = c(7, 8, 9, 10, 11)),
+                                  selectInput("user_slices", "Please select number of chemical substances per slice (Info: The available sample sizes are 49, 56, 63, 70 and 77 and are calculated as the number of independent variables (7 have been chosen in the initial model and in the regression) multiplied with the number of chemical substances per slice (7, 8, 9, 10 or 11)):", choices = c(7, 8, 9, 10, 11)),
                                   textOutput("received_slices")
                                 ),
                                 actionButton("our_model2", "Show results"),
@@ -113,14 +113,14 @@ shinyUI(
                 )
                 )
                 ),
-        tabItem(tabName = "pca", h4("Principal Component Analysis"),
+        tabItem(tabName = "toxpca", h4("Principal Component Analysis (applied to 141 chemicals)"),
                 fluidRow(  column(
                   height = 12,
                   width = 12
                 ),
                   tabBox(width='100%', height='100%',id="tabchart3",
                          tabPanel("PCA results", plotOutput('plot3')),
-                         tabPanel("Percentage of explained variance", plotOutput('plot_4')),
+                         tabPanel("% of explained variance", plotOutput('plot_4')),
                          tabPanel("PCA variables", plotOutput('plot_5')))
                 )
         ),
@@ -152,19 +152,19 @@ shinyUI(
 
 #Literature Sources:
 
-#  [1] J. S. House, F. A. Grimm, W. D. Klaren, A. Dalzell, S. Kuchi, S.-D. Zhang,
+#[1] J. S. House, F. A. Grimm, W. D. Klaren, A. Dalzell, S. Kuchi, S.-D. Zhang,
 #K. Lenz, P. J. Boogaard, H. B. Ketelslegers, T. W. Gant, and et al., “Grouping
 #of uvcb substances with new approach methodologies (nams) data,” ALTEX -
 #  Alternatives to animal experimentation, vol. 38, p. 123–137, Jan. 2021.
 
 #[2] F. Sewell, C. Alexander-White, S. Brescia, R. A. Currie, R. Roberts, C. Roper,
 #C. Vickers, C. Westmoreland, and I. Kimber, “New approach methodologies
-#(nams): identifying and overcoming hurdles to accelerated adoption,” Toxicol-
-#  ogy Research, vol. 13, p. tfae044, 03 2024.
+#(nams): identifying and overcoming hurdles to accelerated adoption,” Toxicology Research, 
+#vol. 13, 03 2024.
 
 #[3] J. F. Fleming, D. L. Filer, D. T. Lloyd, P. Thunga, S. W. Marvel, A. A.
-#Motsinger-Reif, and D. M. Reif, “toxpir: Create toxpi prioritization mod-
-#  els,” 2024d. R package, version 1.3.0, https://cran.r-project.org/web/packages/toxpiR/index.html.
+#Motsinger-Reif, and D. M. Reif, “toxpir: Create toxpi prioritization models,” 2024d. R package,
+#version 1.3.0, https://cran.r-project.org/web/packages/toxpiR/index.html.
 
 #[4] N. Kleinstreuer and T. Hartung, “Artificial intelligence (ai)—it’s the end of
 #the tox as we know it (and i feel fine),” Archives of Toxicology, vol. 98, no. 3,
@@ -180,7 +180,7 @@ shinyUI(
 #  burg, P. Wittkowski, F. Padberg, K. Bech, et al., “Exploring the use of artificial
 #intelligence (ai) for extracting and integrating data obtained through new ap-
 #  proach methodologies (nams) for chemical risk assessment,” EFSA Supporting
-#Publications, vol. 21, no. 1, p. 8567E, 2024.
+#Publications, vol. 21, no. 1, 2024.
 
 #[7] J. F. Fleming, J. S. House, J. R. Chappel, A. A. Motsinger-Reif, and D. M.
 #Reif, “Guided optimization of toxpi model weights using a semi-automated
@@ -195,8 +195,8 @@ shinyUI(
 #metabolomics method to predict high concentration cytotoxicity of drugs from
 #low concentration profiles,” Metabolomics, vol. 8, pp. 433–443, 06 2011.
 
-#[10] C. B. Pestana, J. W. Firman, and M. T. Cronin, “Incorporating lines of ev-
-#  idence from new approach methodologies (nams) to reduce uncertainties in a
+#[10] C. B. Pestana, J. W. Firman, and M. T. Cronin, “Incorporating lines of evidence 
+#from new approach methodologies (nams) to reduce uncertainties in a
 #category based read-across: a case study for repeated dose toxicity,” Regulatory
 #Toxicology and Pharmacology, vol. 120, p. 104855, 2021.
 
@@ -214,7 +214,8 @@ shinyUI(
 #  The National Academies Press, 2014. https://nap.nationalacademies.org/read/18872/chapter/19, accessed on 2025-04-23.
 
 #[14] J. Fleming, D. Filer, D. Lloyd, P. Thunga, S. W. Marvel, A. A. Motsinger-
-#  Reif, and D. M. Reif, “Toxpi r introduction,” 2024b. https://cran.rstudio.com/web/packages/toxpiR/vignettes/introduction.html#:~:text=What%20is%20ToxPi%3F,i.e.%20multimodal%20or%20multiscale%20information, accessed on 2024-11-21.
+#  Reif, and D. M. Reif, “Toxpi r introduction,” 2024b. 
+#https://cran.rstudio.com/web/packages/toxpiR/vignettes/introduction.html#:~:text=What%20is%20ToxPi%3F,i.e.%20multimodal%20or%20multiscale%20information, accessed on 2024-11-21.
 
 #[15] J. Fleming, S. W. Marvel, S. Supak, A. A. Motsinger-Reif, and D. M. Reif,
 #“Toxpi* gis toolkit: creating, viewing, and sharing integrative visualizations
@@ -236,9 +237,9 @@ shinyUI(
 #[19] “Polycyclic aromatic compounds.” National Toxicology Program, U.S. Depart-
 #  ment of Health and Human Services, 2014. https://ntp.niehs.nih.gov/whatwestudy/topics/pacs, accessed on 2024-11-21.
 
-#[20] O. C. Ifegwu and C. Anyakora, “Chapter six - polycyclic aromatic hydrocar-
-#  bons: Part i. exposure,” in Advances in Clinical Chemistry (G. S. Makowski,
-#                                                              ed.), vol. 72, pp. 277–304, Elsevier, 2015.
+#[20] O. C. Ifegwu and C. Anyakora, “Chapter six - polycyclic aromatic hydrocarbons: Part i. exposure,” 
+#in Advances in Clinical Chemistry (G. S. Makowski,
+#ed.), vol. 72, pp. 277–304, Elsevier, 2015.
 
 #[21] A. Schatten, M. Demolsky, D. Winkler, S. Biffl, E. Gostischa-Franta, and
 #T. ¨Ostreicher, Best Practice Software-Engineering. Verlag Heidelberg, 2010.
@@ -253,22 +254,22 @@ shinyUI(
 #D. Hoerl, and E. Miller, readxl: Read Excel Files, 2025. R package version
 #1.4.5, https://cran.r-project.org/web/packages/readxl/index.html.
 
-#[25] S. Karnouskos, R. Sinha, P. Leit˜ao, L. Ribeiro, and T. I. Strasser, “The appli-
-#  cability of iso/iec 25023 measures to the integration of agents and automation
+#[25] S. Karnouskos, R. Sinha, P. Leit˜ao, L. Ribeiro, and T. I. Strasser, 
+#“The applicability of iso/iec 25023 measures to the integration of agents and automation
 #systems,” in IECON 2018 - 44th Annual Conference of the IEEE Industrial
 #Electronics Society, pp. 2927–2934, 2018.
 
 #[26] C. Abras, D. Maloney-Krichmar, and J. Preece, “User-centered design,” User-
 #  Centered Design, pp. 445–456, 01 2004.
 
-#[27] International Organization for Standardization (ISO) and International Elec-
-#  trotechnical Commission (IEC), ISO/IEC 25023:2016 - Systems and software
+#[27] International Organization for Standardization (ISO) and International Electrotechnical Commission (IEC),
+#ISO/IEC 25023:2016 - Systems and software
 #engineering — Systems and software Quality Requirements and Evaluation
 #(SQuaRE) — Measurement of system and software product quality, 2016. On-
 #  line at: https://www.iso.org/standard/35747.html.
 
-#[28] T. Galli, F. Chiclana, and F. Siewe, “Software product quality models, devel-
-#  opments, trends, and evaluation,” SN Computer Science, vol. 1, 05 2020.
+#[28] T. Galli, F. Chiclana, and F. Siewe, “Software product quality models, developments, trends, and evaluation,” 
+#SN Computer Science, vol. 1, 05 2020.
 
 #[29] M. J. Wurm, P. J. Rathouz, and B. M. Hanlon, “Regularized ordinal regression
 #and the ordinalNet R package,” Journal of Statistical Software, vol. 99, no. 6,
@@ -296,58 +297,60 @@ shinyUI(
 
 #[36] M. M. Deza and E. Deza, Encyclopedia of Distances. Springer, 1st ed., 2009.
 
-#[37] D. Jurafsky and J. H. Martin, Speech and Language Processing: An Introduc-
-#  tion to Natural Language Processing, Computational Linguistics, and Speech
-#Recognition with Language Models. published at: https://web.stanford.edu/~jurafsky/slp3/, 3rd ed., 2025. Online manuscript released January 12, 2025.
+#[37] D. Jurafsky and J. H. Martin, Speech and Language Processing: An Introduction to Natural Language 
+#Processing, Computational Linguistics, and Speech
+#Recognition with Language Models. published at: https://web.stanford.edu/~jurafsky/slp3/, 3rd ed., 2025. 
+#Online manuscript released January 12, 2025.
 
-#[38] B. Ripley, B. Venables, D. M. Bates, K. Hornik, A. Gebhardt, and
-#D. Firth, MASS: Support Functions and Datasets for Venables and Ripley’s
-#MASS, 2025. R package version 7.3-64. https://cran.r-project.org/web/packages/MASS/index.html.
+#[38] M. J. Wurm, P. J. Rathouz, and B. M. Hanlon, “ordinalnet: Penalized ordinal regression,” 2022. 
+#R package version 2.12, https://cran.r-project.org/web/packages/ordinalNet/index.html.
 
-#[39] B. Ripley, B. Venables, D. M. Bates, K. Hornik, A. Gebhardt, and D. Firth,
-#“Mass: Support functions and datasets for venables and ripley’s mass,” 2025. R
-#package, version 7.3-64, https://cran.r-project.org/web/packages/MASS/index.html.
+#[39] B. Ripley, B. Venables, D. M. Bates, K. Hornik, A. Gebhardt, and D. Firth, 
+#“Mass: Support functions and datasets for venables and ripley’s mass,” 2025. 
+#R package, version 7.3-64, https://cran.r-project.org/web/packages/MASS/index.html.
 
-#[40] H. Wickham, W. Chang, L. Henry, T. L. Pedersen, K. Takahashi, C. Wilke,
-#K. Woo, H. Yutani, D. Dunnington, and T. van den Brand, ggplot2: Create
-#Elegant Data Visualisations Using the Grammar of Graphics, 2024. R pack-
-#  age version 3.5.1, https://cran.r-project.org/web/packages/ggplot2/index.html.
+#[40] H. Wickham, W. Chang, L. Henry, T. L. Pedersen, K. Takahashi, C. Wilke, 
+#K. Woo, H. Yutani, D. Dunnington, and T. van den Brand, ggplot2: Create Elegant 
+#Data Visualisations Using the Grammar of Graphics, 2024. 
+#R package version 3.5.1, https://cran.r-project.org/web/packages/ggplot2/index.html.
 
-#[41] Y. Xie, J. Cheng, X. Tan, J. Allaire, M. Girlich, G. F. Ellis, J. Rauh, S. Lim-
-#  ited, B. Reavis, L. Gersen, B. Szopka, A. Pickering, W. Holmes, M. Marttila,
-#A. Quintero, and S. Laurent, DT: A Wrapper of the JavaScript Library ’DataT-
-#  ables’, 2024. R version 0.33, https://cran.r-project.org/web/packages/DT/index.html.
+#[41] Y. Xie, J. Cheng, X. Tan, J. Allaire, M. Girlich, G. F. Ellis, J. Rauh, 
+#S. Limited, B. Reavis, L. Gersen, B. Szopka, A. Pickering, W. Holmes, M. Marttila, 
+#A. Quintero, and S. Laurent, DT: A Wrapper of the JavaScript Library ’DataTables’, 
+#2024. R version 0.33, https://cran.r-project.org/web/packages/DT/index.html.
 
-#[42] B. Auguie and A. Antonov, gridExtra: Miscellaneous Functions for ”Grid”
-#Graphics, 2017. R package version 2.3, https://cran.r-project.org/web/packages/gridExtra/index.html.
+#[42] B. Auguie and A. Antonov, gridExtra: Miscellaneous Functions for ”Grid” Graphics, 
+#2017. R package version 2.3, https://cran.r-project.org/web/packages/gridExtra/index.html.
 
-#[43] S. Marvel and D. M. Reif, “Toxpi™ user manual v2.3,” 2018. https://toxpi.org/dist/ToxPi%20User%20Manual.pdf, accessed on 2024-11-21.
+#[43] S. Marvel and D. M. Reif, “Toxpi™ user manual v2.3,” 2018.
+#https://toxpi.org/dist/ToxPi%20User%20Manual.pdf, accessed on 2024-11-21.
 
-#[44] J. Fleming, D. Filer, D. Lloyd, P. Thunga, S. W. Marvel, A. A. Motsinger-Reif,
-#and D. M. Reif, “Manual package ‘toxpir’,” 2024c. https://cran.r-project.org/web/packages/toxpiR/toxpiR.pdf, accessed on 2024-11-21.
+#[44] J. Fleming, D. Filer, D. Lloyd, P. Thunga, S. W. Marvel, 
+#A. A. Motsinger-Reif, and D. M. Reif, “Manual package ‘toxpir’,” 2024c.
+#https://cran.r-project.org/web/packages/toxpiR/toxpiR.pdf, accessed on 2024-11-21.
 
-#[45] A. Kassambara and F. Mundt, “factoextra : Extract and visualize the results
-#of multivariate data analysesg,” 2020. https://cran.r-project.org/web/packages/factoextra/readme/README.html.
+#[45] A. Kassambara and F. Mundt, “factoextra : Extract and visualize the results of 
+#multivariate data analysesg,” 2020. https://cran.r-project.org/web/packages/factoextra/readme/README.html.
 
-#[46] J. R. Groff, P. N. Weinberg, and A. J. Oppel, SQL: The Complete Reference,
-#Third Edition. McGraw-Hill, 2010.
+#[46] J. R. Groff, P. N. Weinberg, and A. J. Oppel, SQL: The Complete Reference, Third Edition. McGraw-Hill, 2010.
 
-#[47] S. Parth, “Understanding k-means and hierarchical clus-
-#  tering in r,” 2020. https://medium.com/@shreytparth/understanding-k-means-and-hierarchical-clustering-in-r-9899342cc4fb,
-#accessed on 2024-11-21.
+#[47] S. Parth, “Understanding k-means and hierarchical clustering in r,” 2020.
+#https://medium.com/@shreytparth/understanding-k-means-and-hierarchical-clustering-in-r-9899342cc4fb, accessed on 2024-11-21.
 
-#[48] Z. Keita, “Introduction to principal component analysis (pca),” 2023. https://www.datacamp.com/tutorial/pca-analysis-r.
+#[48] Z. Keita, “Introduction to principal component analysis (pca),” 2023.
+#https://www.datacamp.com/tutorial/pca-analysis-r.
 
-#[49] A. Kassambara, “Pca - principal component analysis essen-
-#  tials,” 2017. https://www.sthda.com/english/articles/31-principal-component-methods-in-r-practical-guide/112-pca-principal-component-analysis-essentials/.
+#[49] A. Kassambara, “Pca - principal component analysis essentials,” 2017.
+#https://www.sthda.com/english/articles/31-principal-component-methods-in-r-practical-guide/112-pca-principal-component-analysis-essentials/.
 
-#[50] “R shiny tutorial — r shiny dashboard —enabling menu items for their re-
-#  spective pages — r programming.” Data Science Tutorial, 2017. https://www.youtube.com/watch?v=fUXBL5bk20M.
+#[50] “R shiny tutorial — r shiny dashboard —enabling menu items for their
+#respective pages — r programming.” Data ScienceTutorial, 2017. https://www.youtube.com/watch?v=fUXBL5bk20M.
 
-#[51] “Shiny web app tutorial — how to upload a file in shiny app — r programming
-#tutorial.” Data Science Tutorial, 2020. https://www.youtube.com/watch?v=A6VYSCB0TZM&t=199s.
+#[51] “Shiny web app tutorial — how to upload a file in shiny app — r programming tutorial.” 
+#Data Science Tutorial, 2020. https://www.youtube.com/watch?v=A6VYSCB0TZM&t=199s.
 
-#[52] “R shiny basics and user interface.” Statistical Learning Group, 2020. https://www.youtube.com/watch?v=6mJaw5pLtso.
+#[52] “R shiny basics and user interface.” Statistical Learning Group, 2020.
+#https://www.youtube.com/watch?v=6mJaw5pLtso.
 
-#[53] Y. Dai and Y. Xiang, “Connecting sqlite database and shiny app for
-#business intelligence,” 2020. https://shanghai.hosting.nyu.edu/data/r/case-3-sql-shiny.html, accessed on 2024-11-21.
+#[53] Y. Dai and Y. Xiang, “Connecting sqlite database and shiny app for business intelligence,”
+#2020. https://shanghai.hosting.nyu.edu/data/r/case-3-sql-shiny.html, accessed on 2024-11-21.
